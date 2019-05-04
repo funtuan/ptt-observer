806261557cb18e3457fc3098023cb333ff96b6e8
Cat's Schrodinger <hank85627@gmail.com>
2019-05-04 22:18:20 +0800

    Start 取得前20萬篇文章

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const graphqlServer = require('./graphql/server.js');
 const pttBackup = require('./service/pttBackup.js');
 
-// console.log('run');
-// const startTime = new Date();
-// pttBackup.newArticle('Gossiping', 200000-107000, 50, 678100).then(() => {
-//   console.log('總共花費時間', (new Date() - startTime)/1000, '秒');
-// }).catch((err) => {
-//   console.log('error!');
-// });
+console.log('run');
+const startTime = new Date();
+pttBackup.newArticle('Gossiping', 200000, 50, 790680).then(() => {
+  console.log('總共花費時間', (new Date() - startTime)/1000, '秒');
+}).catch((err) => {
+  console.log('error!');
+});
 
 
 // runBackup('Gossiping');
