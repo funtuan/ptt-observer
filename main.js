c9177f2bf76c2a92ebe8017fdc5ec71f38eb1a88
Cat's Schrodinger <hank85627@gmail.com>
2019-05-05 19:04:21 +0800

    run Gossiping last 30000

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const graphqlServer = require('./graphql/server.js');
 const pttBackup = require('./service/pttBackup.js');
 
-console.log('run');
-const startTime = new Date();
-pttBackup.newArticle('Gossiping', 200000-40000, 10, 750000).then(() => {
-  console.log('總共花費時間', (new Date() - startTime)/1000, '秒');
-}).catch((err) => {
-  console.log('error!');
-});
+// console.log('run');
+// const startTime = new Date();
+// pttBackup.newArticle('Gossiping', 200000-40000, 10, 750000).then(() => {
+//   console.log('總共花費時間', (new Date() - startTime)/1000, '秒');
+// }).catch((err) => {
+//   console.log('error!');
+// });
 
 
 // runBackup('Gossiping');
 
 graphqlServer.init();
-// pttBackup.run('Gossiping', 5000, 50);
+pttBackup.run('Gossiping', 30000, 50);
